a70841ab77afd7231cccbf1177394f065e8b17d8
nbonahoo <142938346+nbonahoo@users.noreply.github.com>
2024-11-14 17:45:03 -0500

    working on morseSearch func

--- src/main.c
+++ src/main.c
@@ -1,18 +1,16 @@
 /**
-  ******************************************************************************
-  * @file    main.c
-  * @author  GROUP AWESOME
-  * @date    Nov 1 2024
-  * @brief   ECE 362 Mini project
-  ******************************************************************************
-*/
+ ******************************************************************************
+ * @file    main.c
+ * @author  GROUP AWESOME
+ * @date    Nov 1 2024
+ * @brief   ECE 362 Mini project
+ ******************************************************************************
+ */
 
 #include "stm32f0xx.h"
 #include <string.h>
 
-
-
-//milliseconds
+// milliseconds
 #define SHORT_PRESS 200
 #define LONG_PRESS 800
 
@@ -20,242 +18,249 @@
 #define DASH '-'
 #define END ' '
 
-//need to handle space between letters and space between words 
+// need to handle space between letters and space between words
 
-static char MorseLower[37] = {'a','b', 'c','d','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
-static char MorseUpper[37] = {'A','B', 'C','D','E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
-static char * morse_table[37] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".--", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "..-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----"};
+static char MorseLower[37] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+static char MorseUpper[37] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+static char *morse_table[37] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".--", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "..-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----"};
 
 void init_spi1_slow();
 void nano_wait(unsigned int n);
 void internal_clock();
 
-//track presses
+// track presses
 uint32_t press_start_time = 0;
-uint32_t press_duration   = 0;
-uint8_t  button_pressed   = 0;
+uint32_t press_duration = 0;
+uint8_t button_pressed = 0;
 
-//store morse
+// store morse
 char morse_code[100];
 uint8_t morse_index = 0;
 
 void init_spi1_slow()
 {
-  RCC -> AHBENR |= RCC_AHBENR_GPIOBEN;
+  RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
 
-  RCC ->  APB2ENR |= RCC_APB2ENR_SPI1EN;
+  RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
 
-  GPIOB -> MODER |= GPIO_MODER_MODER3_1 | GPIO_MODER_MODER4_1 | GPIO_MODER_MODER5_1;
-  GPIOB -> AFR[0] |= 0x0;
+  GPIOB->MODER |= GPIO_MODER_MODER3_1 | GPIO_MODER_MODER4_1 | GPIO_MODER_MODER5_1;
+  GPIOB->AFR[0] |= 0x0;
 
-  SPI1 -> CR1 |= SPI_CR1_BR_2 | SPI_CR1_BR_1 | SPI_CR1_BR_0; // SET MAX BAUD RATE
-  SPI1 -> CR1 |= SPI_CR1_MSTR; // MASTER MODE
-  SPI1 -> CR2 |= SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2; // DATA SIZE 8 BITS
+  SPI1->CR1 |= SPI_CR1_BR_2 | SPI_CR1_BR_1 | SPI_CR1_BR_0; // SET MAX BAUD RATE
+  SPI1->CR1 |= SPI_CR1_MSTR;                               // MASTER MODE
+  SPI1->CR2 |= SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2; // DATA SIZE 8 BITS
 
-  SPI1 -> CR1 |= SPI_CR1_SSM;
-  SPI1 -> CR1 |= SPI_CR1_SSI;
+  SPI1->CR1 |= SPI_CR1_SSM;
+  SPI1->CR1 |= SPI_CR1_SSI;
 
-  SPI1 -> CR2 |= SPI_CR2_FRXTH;
+  SPI1->CR2 |= SPI_CR2_FRXTH;
 
-  SPI1 -> CR1 |= SPI_CR1_SPE;
+  SPI1->CR1 |= SPI_CR1_SPE;
 }
 
 void enable_sdcard()
 {
 
-  GPIOB -> BRR |= GPIO_BRR_BR_2;
+  GPIOB->BRR |= GPIO_BRR_BR_2;
 }
 
 void disbale_sdcard()
 {
-  GPIOB -> BSRR |= GPIO_BSRR_BS_2;
+  GPIOB->BSRR |= GPIO_BSRR_BS_2;
 }
 
 void init_sdcard_io()
 {
   init_spi1_slow();
-  GPIOB -> MODER |= GPIO_MODER_MODER2_0;
+  GPIOB->MODER |= GPIO_MODER_MODER2_0;
   disbale_sdcard();
 }
 
 void sdcard_io_high_speed()
 {
-  SPI1 -> CR1 &= ~SPI_CR1_SPE;
-  SPI1 -> CR1 |= SPI_CR1_BR_0; // DIVIDE SO CLOCK RATE IS 12 MHZ
-  SPI1 -> CR1 |= SPI_CR1_SPE;
+  SPI1->CR1 &= ~SPI_CR1_SPE;
+  SPI1->CR1 |= SPI_CR1_BR_0; // DIVIDE SO CLOCK RATE IS 12 MHZ
+  SPI1->CR1 |= SPI_CR1_SPE;
 }
 
 void init_lcd_spi()
 {
-  RCC -> AHBENR |= RCC_AHBENR_GPIOBEN;
-
-  
+  RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
 }
 
-//things may need to change depending on where the button is plugged in 
-void init_button_interrupt() 
-{ 
+// things may need to change depending on where the button is plugged in
+void init_button_interrupt()
+{
   RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
   GPIOA->MODER &= ~GPIO_MODER_MODER0; // PA0 input mode
 
-  RCC-> APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN;
+  RCC->APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN;
 
   SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PA; // EXTI0 for PA0
-  
-  EXTI -> RTSR |= EXTI_RTSR_TR0;
-  EXTI->IMR |= EXTI_IMR_IM0;
-  
+  //SYSCFG -> EXTICR[0] |= SYSCFG_EXTICR1_EXTI2_PA; // EXTI2 FOR PA2
+
+  EXTI-> RTSR |= EXTI_RTSR_TR0;
+  EXTI-> IMR |= EXTI_IMR_IM0;
+
+  //EXTI-> RTSR |= EXTI_RTSR_TR2;
+  //EXTI-> IMR |= EXTI_IMR_IM2;
+
   NVIC_EnableIRQ(EXTI0_1_IRQn);
-  //SysTick_Config(SystemCoreClock/1000);
+  //NVIC_EnableIRQ(EXTI2_3_IRQn);
+  // SysTick_Config(SystemCoreClock/1000);
 }
 
 void init_rgb()
 {
-  RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;
-  GPIOA -> MODER |= GPIO_MODER_MODER5_0;
-  GPIOA -> BSRR |= GPIO_BSRR_BS_10 | GPIO_BSRR_BS_8 | GPIO_BSRR_BS_9;                                                                                                                               
-  GPIOA -> MODER |= GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0 | GPIO_MODER_MODER10_0;
-  GPIOA -> PUPDR |= GPIO_PUPDR_PUPDR10_0 | GPIO_PUPDR_PUPDR8_0 | GPIO_PUPDR_PUPDR9_0;
+  RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+  GPIOA->MODER |= GPIO_MODER_MODER5_0;
+  GPIOA->BSRR |= GPIO_BSRR_BS_10 | GPIO_BSRR_BS_8 | GPIO_BSRR_BS_9;
+  GPIOA->MODER |= GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0 | GPIO_MODER_MODER10_0;
+  GPIOA->PUPDR |= GPIO_PUPDR_PUPDR10_0 | GPIO_PUPDR_PUPDR8_0 | GPIO_PUPDR_PUPDR9_0;
 
-  GPIOA -> MODER |= GPIO_MODER_MODER5_0;
+  GPIOA->MODER |= GPIO_MODER_MODER5_0;
+}
+
+void init_tim7()
+{
+  RCC -> APB1ENR |= RCC_APB1ENR_TIM7EN;
+
+  TIM7 -> PSC = 48000 - 1;
+  TIM7 -> ARR = 1000 - 1;
+
+  TIM7 -> DIER |= TIM_DIER_UIE; // ENABLE TIMER INTERUPT 
+  NVIC_EnableIRQ(TIM7_IRQn);
+
+}
+
+int spaceCounter = 0;
+
+void morseSearch()
+{
+  /*
+  convert morseString in nested loop
+  - look through library
+  - find index of mathcing string
+  - use equal index in Alphabet library
+
+  DMA transfer to display - TFT LCD
+  */
+}
+
+void TIM7_IRQHandler()
+{
+  TIM7 -> SR &= ~TIM_SR_UIF;
+
+  
+  spaceCounter++;
+  if(spaceCounter >= 3)
+  {
+    // Insert space between morse words
+    spaceCounter = 0;
+  }
+
+  morseString = '\0';
+  strindex = 0;
+  togglexn(GPIOA, 5);
+  nano_wait(10000000);
+  togglexn(GPIOA, 5);
+}
+
+void Systick_Handler(void)
+{
+  if (button_pressed)
+    press_duration++;
 }
 
 
-void Systick_Handler(void) { 
-  if(button_pressed)  
-  press_duration++;
-}
+// Global morse string
+char * morseString;
 
-// void EXTI0_1_IRQHandler(void) { 
-//   if(EXTI->PR & EXTI_PR_PR0) {
-//     EXTI->PR |= EXTI_PR_PR0; 
-
-//     if(GPIOC->IDR & GPIO_IDR_0){
-//       button_pressed =1;
-//       press_duration = 0; 
-//     }
-//     else { 
-//       press_duration = 0; 
-
-//       if(press_duration < SHORT_PRESS) { 
-//         // makes sure the super short one is not used 
-//       }
-//       else if (press_duration < LONG_PRESS) {
-//         morse_code[morse_index++] = DOT; 
-//       }else { 
-//         morse_code[morse_index++] = DASH;
-//       }        
-//     }
-
-//   }
-
-// }
-
-int main(void) 
-{ 
+int main(void)
+{
   init_spi1_slow();
   init_lcd_spi();
   init_sdcard_io();
   internal_clock();
-  // init_usart5();
-  // enable_tty_interrupt();
-  // setup_tim1();
   init_button_interrupt();
   init_rgb();
-  //setrgb();
-
+  init_tim7();
+  morseString = (char *)malloc(sizeof(char) * 6);
 }
 
-// EXTI0
-// init_button_interrupt();
-// {
-//     for(;;) {
-//         // Breathe in...
-//         for(float x=1; x<2400; x *= 1.1) {
-//             TIM1->CCR1 = TIM1->CCR2 = TIM1->CCR3 = 2400-x;
-//             nano_wait(100000000);
-//         }
-//         // ...and out...
-//         for(float x=2400; x>=1; x /= 1.1) {
-//             TIM1->CCR1 = TIM1->CCR2 = TIM1->CCR3 = 2400-x;
-//             nano_wait(100000000);
-//         }
-//         // ...and start over.
-//     }
 
-// }
-void togglexn(GPIO_TypeDef *port, int n) 
+void togglexn(GPIO_TypeDef *port, int n)
 {
-  int32_t state; // state of the specified pin
-  state = port -> ODR & (1 << n); // reads ODR value
+  int32_t state;                // state of the specified pin
+  state = port->ODR & (1 << n); // reads ODR value
   if (state)
   {
-    port -> BRR = (1 << n);
+    port->BRR = (1 << n);
   }
   else
   {
-    port -> BSRR = (1 << n);
+    port->BSRR = (1 << n);
   }
-  
 }
+
+int strindex = 0;
 
 void EXTI0_1_IRQHandler()
 {
-  EXTI -> PR = EXTI_PR_PR0;
-  
-  
+  EXTI->PR = EXTI_PR_PR0;
+
+  TIM7 -> CR1 &= ~TIM_CR1_CEN;
 
   uint32_t timer = 0;
-  
- // while(1)
- // {
-    //while((GPIOA -> IDR) & (0xffdf))
-     while(((GPIOA -> IDR) & (GPIO_IDR_0)) != 0)
+
+    
+
+    while(((GPIOA -> IDR) & (GPIO_IDR_0)) != 0)
     {
       timer++;
-      togglexn(GPIOA, 5);
       
     }
-    //togglexn(GPIOA, 5);
-    if(timer <= 2400000)
+    
+    //if (((GPIOA->IDR) & (GPIO_IDR_0)) != 0) {}
+    if (timer <= 600000)
     {
       togglexn(GPIOA, 9);
-      nano_wait(10000000000);
+      nano_wait(200000000);
       togglexn(GPIOA, 9);
-      //break;
+      morseString[strindex] = '.';
+      TIM7 -> CR1 |= TIM_CR1_CEN;
+      TIM7 -> CNT = 0;
     }
     else
     {
-      togglexn(GPIOA,8);
+      togglexn(GPIOA, 8);
       togglexn(GPIOA, 10);
 
-      nano_wait(10000000000);
+      nano_wait(200000000);
 
-      togglexn(GPIOA,8);
+      togglexn(GPIOA, 8);
       togglexn(GPIOA, 10);
-     // break;
+      morseString[strindex] = '-';
+      TIM7 -> CR1 |= TIM_CR1_CEN;
+      TIM7 -> CNT = 0;
     }
-  }
-
 }
 
-
-
-
-void nano_wait(unsigned int n) {
-    asm(    "        mov r0,%0\n"
-            "repeat: sub r0,#83\n"
-            "        bgt repeat\n" : : "r"(n) : "r0", "cc");
+void nano_wait(unsigned int n)
+{
+  asm("        mov r0,%0\n"
+      "repeat: sub r0,#83\n"
+      "        bgt repeat\n" : : "r"(n) : "r0", "cc");
 }
 
 // void init_usart5() {
 //     // TODO
 //     //Enable the RCC clocks to GPIOC and GPIOD.
-//     RCC -> AHBENR |= RCC_AHBENR_GPIOCEN; 
-//     RCC -> AHBENR |= RCC_AHBENR_GPIODEN; 
+//     RCC -> AHBENR |= RCC_AHBENR_GPIOCEN;
+//     RCC -> AHBENR |= RCC_AHBENR_GPIODEN;
 //     //Enable the RCC clock to the USART5 peripheral.
-//     RCC -> APB1ENR |= RCC_APB1ENR_USART5EN; 
-    
+//     RCC -> APB1ENR |= RCC_APB1ENR_USART5EN;
+
 //     //configure pin PC12 to be routed to USART5_TX & USART5_RX
 //     //USART5_TX PC12
 //     GPIOC -> MODER  &= ~(GPIO_MODER_MODER12); //CLear C 12
@@ -277,7 +282,7 @@ void nano_wait(unsigned int n) {
 //     //set no parity control
 //     USART5 -> CR1 &= ~USART_CR1_PCE;
 //     //Use 16x oversampling.
-//     USART5 -> CR1 &= ~USART_CR1_OVER8; 
+//     USART5 -> CR1 &= ~USART_CR1_OVER8;
 //     //Baud rate: 115200
 //     //Family reference = SystemCoreClock
 //     USART5 -> BRR = 48000000 / 115200;
@@ -290,8 +295,6 @@ void nano_wait(unsigned int n) {
 //     while(!(USART5->ISR & USART_ISR_TEACK));
 //     while(!(USART5->ISR & USART_ISR_REACK));
 // }
-
-
 
 // int __io_putchar(int c) {
 //     // TODO
@@ -310,12 +313,10 @@ void nano_wait(unsigned int n) {
 //     // TODO
 //     if (c == '\r') {
 //         c = '\n';
-//     } 
+//     }
 //     __io_putchar(c);
 //     return c;
 // }
-
-
 
 // #include <stdio.h>
 // #include "fifo.h"
@@ -336,7 +337,7 @@ void nano_wait(unsigned int n) {
 //     RCC -> AHBENR |= RCC_AHBENR_DMA2EN;
 //     DMA2 -> CSELR |= DMA2_CSELR_CH2_USART5_RX;
 //     DMA2_Channel2 -> CCR &= ~DMA_CCR_EN;  // First make sure DMA is turned off
-    
+
 //     DMA2_Channel2 -> CMAR = (uint32_t)serfifo;
 //     DMA2_Channel2 -> CPAR = (uint32_t)&(USART5->RDR);
 //     DMA2_Channel2 -> CNDTR = FIFOSIZE;
@@ -355,7 +356,7 @@ void nano_wait(unsigned int n) {
 // char interrupt_getchar() {
 //     // TODO
 //     while (fifo_newline(&input_fifo) == 0) {
-//         asm volatile ("wfi");  
+//         asm volatile ("wfi");
 //     }
 //     return fifo_remove(&input_fifo);
 // }
@@ -387,15 +388,13 @@ void nano_wait(unsigned int n) {
 // }
 // // TODO Remember to look up for the proper name of the ISR function
 
-
-
 // void setup_tim1(void) {
 //     // Generally the steps are similar to those in setup_tim3
-//     // except we will need to set the MOE bit in BDTR. 
+//     // except we will need to set the MOE bit in BDTR.
 //     // Be sure to do so ONLY after enabling the RCC clock to TIM1.
-//     RCC->AHBENR |= RCC_AHBENR_GPIOAEN; 
+//     RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 //     RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
-    
+
 //     //GPIOA->MODER &= ~(0xFF0000);
 //     GPIOA->MODER |= 0xAA0000;
 
@@ -409,8 +408,8 @@ void nano_wait(unsigned int n) {
 
 //     TIM1 -> CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2; //8
 //     TIM1 -> CCMR1 |= TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2; //9
-//     //TIM1 -> CCMR1 &= ~(TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC2M_0); //CLEAR 
-    
+//     //TIM1 -> CCMR1 &= ~(TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC2M_0); //CLEAR
+
 //     TIM1 -> CCMR2 |= TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC3M_2; //10
 //     TIM1 -> CCMR2 |= TIM_CCMR2_OC4M_1 | TIM_CCMR2_OC4M_2; //11
 //     //TIM1 -> CCMR2 &= ~(TIM_CCMR2_OC3M_0 | TIM_CCMR2_OC4M_0);
@@ -421,12 +420,11 @@ void nano_wait(unsigned int n) {
 //     TIM1 -> CCER |= TIM_CCER_CC2E;
 //     TIM1 -> CCER |= TIM_CCER_CC3E;
 //     TIM1 -> CCER |= TIM_CCER_CC4E;
-    
+
 //     TIM1->CR1 |= TIM_CR1_CEN;
 // }
 
-//int getrgb(void);
-
+// int getrgb(void);
 
 // Helper function for you
 // Accept a byte in BCD format and convert it to decimal
@@ -445,10 +443,10 @@ void nano_wait(unsigned int n) {
 //     uint8_t b = bcd2dec(rgb & 0xFF);
 //     uint8_t g = bcd2dec((rgb >> 8) & 0xFF);
 //     uint8_t r = bcd2dec((rgb >> 16) & 0xFF);
-    
-//     int blue  = (100 - b) * volume / 100; 
+
+//     int blue  = (100 - b) * volume / 100;
 //     int green = (100 - g) * volume / 100;
-//     int red   = (100 - r) * volume / 100; 
+//     int red   = (100 - r) * volume / 100;
 //     // TODO: Assign values to TIM1->CCRx registers
 //     // Remember these are all percentages
 //     TIM1 -> CCR1 = red;
@@ -457,5 +455,3 @@ void nano_wait(unsigned int n) {
 //     // Also, LEDs are on when the corresponding PWM output is low
 //     // so you might want to invert the numbers.
 // }
-
-
